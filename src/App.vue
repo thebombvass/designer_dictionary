8f4ec7623daea0dfad6c6400144647ef45dc1826
thebombvass <mevomvas@bu.edu>
2019-09-19 14:16:23 +0200

    built out listing-page component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
   <div id="app">
   <header>
-    The Fashion App is this
+    <div class="navbar-expand-sm">
+    <nav class="navbar navbar-expand-lg fixed top">
+      <a class="navbar-brand" href="#">Designer Dictionary</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <div class="collapse navbar-collapse container-fullwidth" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Brands<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Shows</a>
+          </li>
+        </ul>
+        <!-- <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form> -->
+      </div>
+    </nav>
+    </div>
   </header>
+  <listing-page></listing-page>
 
   </div>
 </template>
@@ -21,7 +44,54 @@ export default {
 </script>
 
 <style>
+/* color scheme
+Rich Black - #141414;
+White - #FFFFFF;
+Melon - #FFBEAA; dont use
+Mimi Pink - rgb(255, 202, 214);
+Sinopia - #EA410E;
+*/
+
+header {
+  position: sticky; /*fix */
+}
+
+nav {
+  width: 100%;
+}
+
+.navbar {
+  background-color: #141414;
+  padding: 0;
+}
+.navbar-collapse {
+    background-color: rgb(253, 226, 226);
+    width: 100%;
+  } 
+
+.nav-item {
+  border: 2px solid white;
+}
+
+.navbar a {
+  color: rgb(255, 202, 214);
+  margin: 3%;
+}
+
+.navbar-toggler {
+  background-color: rgb(255, 203, 215);
+  margin: 4%;
+}
+
+.fa-bars{
+  color: #141414;
+}
+
 #app {
   font-family: Arial, Helvetica, sans-serif;
+}
+
+#navbarSupportedContent a {
+  color: black;
 }
 </style>
--- src/components/ListingPage.vue
+++ src/components/ListingPage.vue
@@ -1,21 +1,171 @@
 <template>
-    <div>
-        Tis Listing Page nowz
+    <div id="listingPage">
+    
+    <a class="btn btn-light filter-btn" data-toggle="collapse" href="#filters" collapsed aria-controls="collapseExample">
+        Filter content
+    </a>
+
+    <div id="filters">
+        <div>
+        <form>
+            <div class="form-group">
+                <select class="form-control" id="selecter">
+                    <option value="all" selected>Sort By...</option>
+                    <option value ="AZ">Brand Name A-Z</option>
+                    <option value ="ZA">Brand Name Z-A</option>
+                    <option value ="old">Year Founded (oldest)</option>
+                    <option value ="new">Year Founded (newest)</option>
+                </select>
+                <p>Filter brands by country they were founded in:</p>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="IT">
+                    <label class="form-check-label" for="inlineCheckbox1">Italian</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="FR">
+                    <label class="form-check-label" for="inlineCheckbox2">French</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="US">
+                    <label class="form-check-label" for="inlineCheckbox3">American</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="UK">
+                    <label class="form-check-label" for="inlineCheckbox3">English</label>
+                </div>
+            </div>
+        </form>
+        </div>
+    </div>
+
+    <div id="content">
+        <ul class="list-unstyled">
+            <li v-for="brand in brands" v-bind:key="brand.name">
+                <img v-bind:src="brand.brand_logo"> 
+                <span>{{brand.name}}</span>
+            </li>
+        </ul>
+    </div>
+
     </div>
 </template>
 
 
 <script>
 import MoreDetails from './MoreDetails.vue'
+import json from '../assets/designDictData.json'
+
 
 export default {
     name: 'ListingPage',
     components: {
         MoreDetails,
+    },
+    data() {
+        return {
+            brands: json.brands,
+            viaJS: '/assets/prada_logo.jpg'
+        }
+    },
+    computed: {
+        names() {
+            return json.brands.map((item) => {
+            return item.name;
+            })
+        },
+        brand_logo() {
+            return json.brands.map((item) => {
+            return '/assets/' + item.brand_logo
+            })
+        },
+        year_founded() {
+            return json.brands.map((item) => {
+            return item.year_founded;
+            })
+        },
+        city_founded() {
+            return json.brands.map((item) => {
+            return item.city_founded;
+            })
+        },
+        pronounciation() {
+            return json.brands.map((item) => {
+            return item.pronounciation;
+            })
+        },
+        designers() {
+            return json.brands.map((item) => {
+            return item.designers;
+            })
+        },
+        site_url() {
+            return json.brands.map((item) => {
+            return item.site_url;
+            })
+        },
+        bio() {
+            return json.brands.map((item) => {
+            return item.bio;
+            })
+        },
     }
 }
 </script>
 
 <style scoped>
+
+form {
+    padding: 10px;
+}
+
+li {
+    border: 2px solid black;
+    margin: 15px;
+    margin-bottom: 20px;
+    background-color: white;
+}
+
+li img {
+    width: 25%;
+    margin: 5px;
+}
+
+li span {
+    font-size: 20px;
+    margin-left: 5px;
+}
+
+.filter-btn {
+    font-size: 18px;
+    margin-top: 20px;
+    width: 90%;
+    margin-left: 5%;
+    height: 40px;
+}
+
+#content {
+  position: relative;
+}
+
+#filters {
+  position: absolute;
+  z-index: 1;
+}
+
+#filters {
+    width: 90%;
+    margin-left: 5%;
+    background-color: rgb(216, 222, 230);
+}
+
+#selecter {
+    -webkit-appearance: none;
+    background-image: url("data:image/svg+xml;charset=UTF-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%2212%22%20height%3D%2212%22%20viewBox%3D%220%200%2012%2012%22%3E%3Ctitle%3Edown-arrow%3C%2Ftitle%3E%3Cg%20fill%3D%22%23000000%22%3E%3Cpath%20d%3D%22M10.293%2C3.293%2C6%2C7.586%2C1.707%2C3.293A1%2C1%2C0%2C0%2C0%2C.293%2C4.707l5%2C5a1%2C1%2C0%2C0%2C0%2C1.414%2C0l5-5a1%2C1%2C0%2C1%2C0-1.414-1.414Z%22%20fill%3D%22%23000000%22%3E%3C%2Fpath%3E%3C%2Fg%3E%3C%2Fsvg%3E");
+    background-size: .6em;
+    background-position: calc(100% - 1.3em) center;
+    background-repeat: no-repeat;
+    width: 80%;
+    margin-bottom: 15px;
+}
 
 </style>
